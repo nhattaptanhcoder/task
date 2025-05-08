e8fbb1e1e5e91c8a2dbdb7813a3f81fd8bb70425
Ngoc Nhat Dep Trai <nhats987650@gmail.com>
2025-05-08 11:16:05 +0700

    Task5

---
+++ task5.js
@@ -0,0 +1,32 @@
+let n  ,m ;
+
+function checkPrime(num)
+{
+    if (num <= 1) return false; 
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false; 
+    }
+    return true; 
+
+}
+
+function Prime(n,m){
+    let arrStore = [];
+    for (let i = n ; i<= m ; i++){
+        if (checkPrime(i)){
+            arrStore.push(i);
+        }
+    }
+    return arrStore;
+}
+co
+let sumPrime = function (n,m) {
+    let arr = Prime(n,m);
+    let sum = 0;
+    arr.forEach(element => {
+        sum += element;
+    });
+    return sum;
+}
+
+console.log(sumPrime(n,m));
